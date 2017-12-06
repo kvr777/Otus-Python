3b8ec3d64a9154f1e82a8d2a61e70c5c9d2c9dc5
kvr777 <kirill.v.romanov@gmail.com>
2017-12-06 13:40:44 +0100

    The changes are: 1. Rules for logging parameter was changed 2. Change parse_log function. Now it is possible to have some number of bad lines in log. We control it by special parameter in config. If number of bad lines is exceeded, the script will be stopped 3. Optimize generate_html_report function 4. Other minor changes

--- W1_Log_Analyzer/log_analyzer.py
+++ W1_Log_Analyzer/log_analyzer.py
@@ -9,6 +9,7 @@
 
 import os
 import re
+import sys
 import argparse
 import configparser
 import gzip
@@ -22,8 +23,9 @@ def_config = {
     "REPORT_SIZE": 555,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log",
-    "LOGGING": "./monitoring",
-    "TSFILE": "./monitoring"
+    "LOGGING": "log_analyzer.log",
+    "TSFILE": "./monitoring",
+    "ERR_PARSE_RATE": 10
     }
 
 
@@ -31,8 +33,8 @@ def parse_config (config, config_path):
     # update default config from file
     file_config = configparser.ConfigParser()
     file_config.read(config_path)
-    file_conf_args = [key for key in file_config['MAIN']]
-    for file_conf_arg in file_conf_args:
+
+    for file_conf_arg in file_config['MAIN'].keys():
         config[file_conf_arg.upper()] = file_config['MAIN'][file_conf_arg]
 
     # try to parse config. Though we pass dict as a result, it is useful to discover missing parameters in early stage
@@ -40,8 +42,9 @@ def parse_config (config, config_path):
         report_size = int(config['REPORT_SIZE'])
         report_dir = config['REPORT_DIR']
         log_dir = config['LOG_DIR']
-        logging_dir = config['LOGGING'] if 'LOGGING' in [key for key in config.keys()] else None
+        logging_dir = config['LOGGING']
         ts_file_dir = config['TSFILE']
+        err_parce_rate = int(config['ERR_PARSE_RATE'])
         return config
     except:
         raise
@@ -60,56 +63,65 @@ def find_last_log(log_dir):
     """
 
     # find the log files by mask in name. Then select one with the last date
-    last_log_features = namedtuple('last_log_features', ['last_log', 'last_log_date'])
+    LastLogFeatures = namedtuple('LastLogFeatures', ['last_log', 'last_log_date'])
     mask = 'nginx-access-ui.log-'
 
     last_log = None
     last_log_date = 0
 
-    for file in os.listdir(log_dir):
-        if file.startswith(mask):
-            log_temp = re.search(str(mask)+'(\d{4}\d{2}\d{2})\.?', file)
+    for dir_file in os.listdir(log_dir):
+        if dir_file.startswith(mask):
+            log_temp = re.search(mask+'(\d{4}\d{2}\d{2})\.?', dir_file)
             if log_temp is not None:
                 if int(log_temp.group(1)) > last_log_date:
-                    last_log = file
+                    last_log = dir_file
                     last_log_date = int(log_temp.group(1))
-    last_log_features.last_log = last_log
-    last_log_features.last_log_date = last_log_date
+    last_log_features = LastLogFeatures(last_log, last_log_date)
     return last_log_features
 
 
-def parse_log(last_log_with_path):
+def parse_log(last_log_with_path, err_parse_rate):
     """ This func retrieve an url and its request time
     At the first step, depending on file format, we select a function for file opening (gz or standard).
     Then we process each line of the log and retrieve url(7-th column) and time (the last column) from the log.
     Args:
         last_log_with_path: path to logfile with the latest date in the name
+        err_parse_rate: maximum number of wrong lines in log. If this threshold exceeds the execution will be stopped
 
     Returns:
         time_log: dict with url as a key and list of times as a values
     """
     if last_log_with_path.endswith(".gz"):
-        myfile = gzip.open(last_log_with_path, 'rb')
+        last_log_file = gzip.open(last_log_with_path, 'rb')
     else:
         try:
-            myfile = open(last_log_with_path,  'rb')
+            last_log_file = open(last_log_with_path,  'rb')
         except:
-            logging.error("An Error Occurred While Opening the Log File")
+            logging.error("An error occurred while opening the log file")
             raise
     time_log = defaultdict(list)
-    try:
-        for line in myfile:
-            splitted = line.split()
-            url = splitted[6].decode("utf-8")
-            url = url.strip('"')
-            response_time = splitted[-1].decode("utf-8")
-            time_log[url].append(response_time)
-        # Closes the file
-        myfile.close()
-        return time_log
-    except:
-        logging.error("Error while parsing log. Please check this logfile: {}".format(last_log_with_path))
-        raise
+    curr_parce_err = 0
+    for line in last_log_file:
+        if curr_parce_err < int(err_parse_rate):
+            try:
+                splitted = line.split()
+                url = splitted[6].decode("utf-8")
+                url = url.strip('"')
+                response_time = splitted[-1].decode("utf-8")
+                time_log[url].append(response_time)
+            except:
+                curr_parce_err += 1
+        else:
+            # Closes the file
+            last_log_file.close()
+            logging.error("Number of wrong lines in the file exceeded defined threshold. "
+                          "Script execution will be stopped ")
+            sys.exit()
+    # Closes the file
+    last_log_file.close()
+    return time_log
+
+
 
 def create_report(time_log, report_size):
 
@@ -180,7 +192,9 @@ def create_report(time_log, report_size):
 
     return filtered_report
 
+
 def generate_html_report (filtered_report, report_dir,  last_report_name):
+
     """ Function to generate html report
 
     Args:
@@ -192,32 +206,24 @@ def generate_html_report (filtered_report, report_dir,  last_report_name):
         True if function successfully executed. Raise exception otherwise
     """
 
-    # copy html template and create html file with '_temp' mask
     try:
-        copy2('report.html', os.path.join(report_dir, str('temp_') + last_report_name))
+        # open temporary html file and copy his content
+        with open('report.html', 'r', encoding='utf-8') as html_template:
+            html_data = html_template.read()
     except:
         logging.error("Report template not found")
         raise
-
     try:
-        # open temporary html file and copy his content
-        with open(os.path.join(report_dir, str('temp_') + last_report_name), 'r', encoding='utf-8') as html_report:
-            html_data = html_report.read()
-
         # replace '$table_json' placeholder by the data from filtered_report variable
         newdata = html_data.replace('$table_json', str(filtered_report))
 
         # open temporary html file and inject report data
-        with open(os.path.join(report_dir, str('temp_') + last_report_name), 'w') as html_report:
+        with open(os.path.join(report_dir, last_report_name), 'w') as html_report:
             html_report.write(newdata)
 
-        # if all was ok, remove temp_ mask from report's filename
-        os.rename(os.path.join(report_dir, str('temp_') + last_report_name),
-                  os.path.join(report_dir, last_report_name))
-
-        logging.info("New Report Has Been Generated")
+        logging.info("New report has been generated")
     except:
-        logging.error("An Error Occurred While Creating the html-report")
+        logging.error("An error occurred while creating the html-report")
 
 
 def generate_ts_file(ts_file_dir):
@@ -236,34 +242,33 @@ def generate_ts_file(ts_file_dir):
             file.write(ts)
         logging.info("log_analyzer.ts has been successfully updated")
     except:
-        logging.error("An Error Occurred While Generating log_analyzer.ts")
+        logging.error("An error occurred while generating log_analyzer.ts")
         raise
 
 
 def main(config):
 
-
     # Find the last log file. if file wasn't found, handle this situation
     last_log_features = find_last_log(config['LOG_DIR'])
 
     if not last_log_features.last_log:
-        return logging.info("Any Logfile Wasn't Found In Log Directory")
+        return logging.info("Any logfile wasn't found in log directory")
 
     # check does html report already exist and handle these situations
     last_report_name = 'report_' + str(last_log_features.last_log_date) + '.html'
 
     if os.path.isfile(os.path.join(config['REPORT_DIR'], last_report_name)):
-        return logging.info("Last Log Report Already Exists. Script Execution Will Be Stopped")
+        return logging.info("Last log report already exists. Script execution will be stopped")
     else:
-        logging.info("The Last Log Has Been Found. Report Creation Process Will Be Initiated")
+        logging.info("The last log has been found. Report creation process will be initiated")
 
     # extract data from the last log file.
-    time_log = parse_log(os.path.join(config['LOG_DIR'], last_log_features.last_log))
-    logging.info("Last Log Data Has Been Extracted")
+    time_log = parse_log(os.path.join(config['LOG_DIR'], last_log_features.last_log), config['ERR_PARSE_RATE'])
+    logging.info("Last log data has been extracted")
 
     # Generate report data
     filtered_report = create_report(time_log, config['REPORT_SIZE'])
-    logging.info("Reports' Data Has Been Generated")
+    logging.info("Reports' data has been generated")
 
     # Get html template, copy the report data and generate the html-report
     generate_html_report(filtered_report, config['REPORT_DIR'], last_report_name)
@@ -271,11 +276,10 @@ def main(config):
     # ts-file generation
     generate_ts_file(config['TSFILE'])
 
+
 if __name__ == "__main__":
 
-    config = def_config
     # set up arguments and argument parser
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--config',
                         help='Path to configuration file. Please use path+filename notation',
@@ -284,11 +288,10 @@ if __name__ == "__main__":
     # parse config file
     args = parser.parse_args()
     config_path = args.config
-    config = parse_config(config, config_path)
+    config = parse_config(def_config, config_path)
 
     # set up logging. If directory for logging is not defined, use stdout
-    logging.basicConfig(filename=os.path.join(config['LOGGING'], 'log_analyzer.log')
-                        if 'LOGGING' in [key for key in config.keys()] else None,
+    logging.basicConfig(filename=config['LOGGING'] if len(config['LOGGING']) > 4 else None,
                         level=logging.INFO,
                         format='[%(asctime)s] %(levelname).1s %(message)s',
                         datefmt='%Y.%m.%d %H:%M:%S')
