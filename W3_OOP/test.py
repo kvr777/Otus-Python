479002c5b396676f67ef11be14060395e87ac39a
kvr777 <kirill.v.romanov@gmail.com>
2017-12-11 21:36:57 +0100

    initial commit

---
+++ W3_OOP/test.py
@@ -0,0 +1,21 @@
+import unittest
+
+import api
+
+
+class TestSuite(unittest.TestCase):
+    def setUp(self):
+        self.context = {}
+        self.headers = {}
+        self.store = None
+
+    def get_response(self, request):
+        return api.method_handler({"body": request, "headers": self.headers}, self.context, self.store)
+
+    def test_empty_request(self):
+        _, code = self.get_response({})
+        self.assertEqual(api.INVALID_REQUEST, code)
+
+
+if __name__ == "__main__":
+    unittest.main()
