122963ad31804af6c7b2406d6b4e78267ec5db1f
kvr777 <kirill.v.romanov@gmail.com>
2018-03-06 14:26:23 +0700

    -refactor code -batch processing for queue instead of single row processing -memcached connections are stored in dict and reconnect only if error

--- W12_Concurrency/memc_load.py
+++ W12_Concurrency/memc_load.py
@@ -17,6 +17,7 @@ import memcache
 import threading
 import multiprocessing as mp
 import queue
+import time
 
 logging.basicConfig(filename=None, level=logging.INFO,
                     format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
@@ -25,17 +26,30 @@ logging.basicConfig(filename=None, level=logging.INFO,
 NORMAL_ERR_RATE = 0.01
 CONNECTION_TIMEOUT = 1
 CONNECTION_RETRIES = 5
+WORKER_BATCH_SIZE = 100
+READ_LOG_SIZE = 200000
+WRITE_LOG_SIZE = 10000
 
 AppsInstalled = collections.namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
 
 lock = threading.Lock()
-
+memc_conn_dict = {}
 
 def dot_rename(path):
     head, fn = os.path.split(path)
     # atomic in most cases
     # t = str(int(time.time()))+'.'
-    os.rename(path, os.path.join(head, "." + fn))
+    # os.rename(path, os.path.join(head, t+fn))
+    # os.rename(path, os.path.join(head, "." + fn))
+
+
+def get_packed (appsinstalled):
+    ua = appsinstalled_pb2.UserApps()
+    ua.lat = appsinstalled.lat
+    ua.lon = appsinstalled.lon
+    key = "%s:%s" % (appsinstalled.dev_type, appsinstalled.dev_id)
+    ua.apps.extend(appsinstalled.apps)
+    return ua.SerializeToString()
 
 
 def insert_appsinstalled(memc_addr, appsinstalled, dry_run=False):
@@ -45,17 +59,16 @@ def insert_appsinstalled(memc_addr, appsinstalled, dry_run=False):
     key = "%s:%s" % (appsinstalled.dev_type, appsinstalled.dev_id)
     ua.apps.extend(appsinstalled.apps)
     packed = ua.SerializeToString()
-    # @TODO persistent connection
-    # @TODO retry and timeouts!
 
     if dry_run:
         logging.debug("%s - %s -> %s" % (memc_addr, key, str(ua).replace("\n", " ")))
     else:
         counter = 0
-        memc = None
+        memc = memc_conn_dict.get(memc_addr, None)
         while (counter <= CONNECTION_RETRIES) & (not memc):
             try:
                 memc = memcache.Client([memc_addr], socket_timeout=CONNECTION_TIMEOUT)
+                memc_conn_dict[memc_addr] = memc
             except Exception as e:
                 logging.warning("Connection Failed **BECAUSE:** {}".format(e))
                 logging.info("Attempt {} of 100".format(counter))
@@ -65,6 +78,7 @@ def insert_appsinstalled(memc_addr, appsinstalled, dry_run=False):
         else:
             try:
                 memc.set(key, packed)
+                memc.get(key)
             except Exception as e:
                 logging.exception("Cannot write to memc %s: %s" % (memc_addr, e))
                 return False
@@ -91,15 +105,23 @@ def parse_appsinstalled(line):
 
 
 def do_work(memc_addr, input_q, result_q, dry_run):
+    w_process = w_error = 0
     while True:
-        item = input_q.get()
-        ok = insert_appsinstalled(memc_addr, item, dry_run)
-        result_q.put(ok)
+        try:
+            items = input_q.get(timeout=1)
+        except queue.Empty:
+            result_q.put([w_process, w_error])
+            logging.info('Finally processed {} rows in address {}'.format((w_process + w_error), memc_addr))
+            return
+        for item in items:
+            ok = insert_appsinstalled(memc_addr, item, dry_run)
+            if ok:
+                w_process += 1
+            else:
+                w_error += 1
+            if (w_process + w_error) % WRITE_LOG_SIZE == 0:
+                logging.info('Processed {} rows in address {}'.format((w_process + w_error), memc_addr))
         input_q.task_done()
-        q_size = result_q.qsize()
-        if q_size % 10000 == 0:
-            logging.info('Processed {} rows in address {}'.format(q_size, memc_addr))
-
 
 def process_file(options, fn):
     device_memc = {
@@ -111,10 +133,12 @@ def process_file(options, fn):
 
     processed = errors = 0
     workers_queue_dict = {}
+    line_batch_dict = {}
 
     for key in device_memc.keys():
         # create queue for writing rows in memcache and queue to store the results of writing
         workers_queue_dict[key] = (queue.Queue(), queue.Queue())
+        line_batch_dict[key] = list()
 
         # launch thread for every thread
         t = threading.Thread(target=do_work,
@@ -127,6 +151,7 @@ def process_file(options, fn):
     logging.info('Processing %s' % fname)
     fd = gzip.open(fn, 'rt')
     a = 0
+
     for line in fd:
         line = line.strip()
         if not line:
@@ -141,16 +166,29 @@ def process_file(options, fn):
             logging.error("Unknown device type: %s" % appsinstalled.dev_type)
             continue
 
-        workers_queue_dict.get(appsinstalled.dev_type)[0].put(appsinstalled)
+        line_batch_dict.get(appsinstalled.dev_type).append(appsinstalled)
+
+        if len(line_batch_dict.get(appsinstalled.dev_type)) >= WORKER_BATCH_SIZE:
+            appsinstalled_list = line_batch_dict.get(appsinstalled.dev_type)
+            workers_queue_dict.get(appsinstalled.dev_type)[0].put(appsinstalled_list)
+            line_batch_dict[appsinstalled.dev_type] = []
+
         a += 1
-        if a % 500000 == 0:
-            logging.info('Processed {} rows'.format(a))
+        if a % READ_LOG_SIZE == 0:
+            logging.info('Read {} rows in file {}'.format(a, fname))
+            # workers_queue_dict.get(appsinstalled.dev_type)[0].put(appsinstalled)
+
+    for key in line_batch_dict.keys():
+        appsinstalled_list = line_batch_dict.get(key)
+        if len(appsinstalled_list):
+            workers_queue_dict.get(key)[0].put(appsinstalled_list)
+            line_batch_dict[key] = []
 
     for key in workers_queue_dict.keys():
         workers_queue_dict.get(key)[0].join()
-        res_list = list(workers_queue_dict.get(key)[1].queue)
-        processed += res_list.count(True)
-        errors += res_list.count(False)
+        w_line_process = workers_queue_dict.get(key)[1].get()
+        processed += w_line_process[0]
+        errors += w_line_process[1]
 
     if not processed:
         fd.close()
@@ -165,21 +203,15 @@ def process_file(options, fn):
     return fn
 
 
-def main_multiprocessing(options):
-
-    files_to_process = glob.iglob(options.pattern)
-
-    with mp.Pool(int(options.w)) as p:
-        for x in p.imap(partial(process_file, options), files_to_process):
-            dot_rename(x)
-
-
 def main(options):
 
     logging.info("Memc loader started with options: %s" % options)
+    files_to_process = glob.iglob(options.pattern)
 
     try:
-        main_multiprocessing(options)
+        with mp.Pool(int(options.w)) as p:
+            for x in p.imap(partial(process_file, options), files_to_process):
+                dot_rename(x)
 
     except Exception as e:
         logging.exception("Unexpected error: %s" % e)
